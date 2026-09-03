e94789c3b59832a3bf88ec301c9756fbdddead24
shaikhanees841 <shaikhanees841@gmail.com>
2026-09-03 23:09:14 +0530

    docs: remove AI feature reference from README

--- smartcart-backend/src/main/java/com/smartcart/config/SwaggerConfig.java
+++ smartcart-backend/src/main/java/com/smartcart/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.smartcart.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
